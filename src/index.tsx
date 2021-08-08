7344e46c6ca66314161037521b4e649e82d5591d
Lucas Botteri <lucasbotteri@hotmail.com>
2021-08-08 00:47:42 -0300

    Set up component Library

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./app/store";
 import * as serviceWorker from "./serviceWorker";
+import "./index.less";
 
 ReactDOM.render(
   <React.StrictMode>
